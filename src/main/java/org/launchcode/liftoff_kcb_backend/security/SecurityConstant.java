9e3c67e182ff73ed031e111d6f812a20be3529b7
brandonwebdev <brandonwebdev@proton.me>
2022-12-26 08:59:12 -0600

    increase jwt expiration time to an hour

--- src/main/java/org/launchcode/liftoff_kcb_backend/security/SecurityConstant.java
+++ src/main/java/org/launchcode/liftoff_kcb_backend/security/SecurityConstant.java
@@ -1,6 +1,8 @@
 package org.launchcode.liftoff_kcb_backend.security;
 
+import java.time.temporal.ChronoUnit;
+
 public class SecurityConstant {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = ChronoUnit.HOURS.getDuration().toMillis();    // 1 hour
     public static final String JWT_SECRET = "secret";
 }
