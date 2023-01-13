a780d5da9ddd61d7405c60cac7745bc799ed6715
brandonwebdev <brandonwebdev@proton.me>
2023-01-13 07:56:48 -0600

    move jwt secret to app properties, closes #8

--- src/main/java/org/launchcode/liftoff_kcb_backend/security/SecurityConstant.java
+++ src/main/java/org/launchcode/liftoff_kcb_backend/security/SecurityConstant.java
@@ -4,5 +4,4 @@ import java.time.temporal.ChronoUnit;
 
 public class SecurityConstant {
     public static final long JWT_EXPIRATION = ChronoUnit.HOURS.getDuration().toMillis();    // 1 hour
-    public static final String JWT_SECRET = "secret";
 }
